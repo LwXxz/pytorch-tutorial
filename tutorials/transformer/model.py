44dcad2d8a312b60bcd4ba395fd309e7a4060361
LwXxz <lwxxzcc@gmail.com>
2022-12-26 13:10:44 +0800

    update Transformer

--- tutorials/transformer/model.py
+++ tutorials/transformer/model.py
@@ -4,14 +4,36 @@ import torch.nn.functional as F
 
 import numpy as np
 
-src_len = 5 # length of source
-tgt_len = 5 # length of target
+def make_batch():
+  input_batch = [[src_vocab[n] for n in sentences[0].split()]]
+  output_batch = [[tgt_vocab[n] for n in sentences[1].split()]]
+  target_batch = [[tgt_vocab[n] for n in sentences[2].split()]]
+  return torch.LongTensor(input_batch), torch.LongTensor(output_batch), torch.LongTensor(target_batch)
 
-d_model = 512  # Embedding Size
-d_ff = 2048  # FeedForward dimension
-d_k = d_v = 64  # dimension of K(=Q), V
-n_layers = 6  # number of Encoder of Decoder Layer
-n_heads = 8  # number of heads in Multi-Head Attention
+def get_sinusoid_encoding_table(n_position, d_model):
+  def cal_angle(position, hid_idx):
+      return position / np.power(10000, 2 * (hid_idx // 2) / d_model)
+  def get_posi_angle_vec(position):
+      return [cal_angle(position, hid_j) for hid_j in range(d_model)]
+
+  sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in range(n_position)])
+  sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+  sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+  return torch.FloatTensor(sinusoid_table)
+
+def get_attn_pad_mask(seq_q, seq_k):
+  # print(seq_q)
+  batch_size, len_q = seq_q.size()
+  batch_size, len_k = seq_k.size()
+  # eq(zero) is PAD token
+  pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
+  return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
+
+def get_attn_subsequent_mask(seq):
+  attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
+  subsequent_mask = np.triu(np.ones(attn_shape), k=1)
+  subsequent_mask = torch.from_numpy(subsequent_mask).byte()
+  return subsequent_mask
 
 class MLP(nn.Module):
     def __init__(self, in_channels, out_channels, num_layers, hidden_channels):
@@ -151,3 +173,37 @@ class Transformer(nn.Module):
         decoder_logit = self.linear(decoder_output)
         return decoder_logit.view(-1, decoder_logit.size(-1)), encoder_self_attn, decoder_self_attention, decoder_encoder_attention
 
+if __name__ == '__main__':
+  sentences = ['ich mochte ein bier P', 'S i want a beer', 'i want a beer E']
+  # Transformer Parameters
+  # Padding Should be Zero index
+  src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4}
+  src_vocab_size = len(src_vocab)
+
+  tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'S': 5, 'E': 6}
+  number_dict = {i: w for i, w in enumerate(tgt_vocab)}
+  tgt_vocab_size = len(tgt_vocab)
+
+  src_len = 5 # length of source
+  tgt_len = 5 # length of target
+
+  d_model = 512  # Embedding Size
+  d_ff = 2048  # FeedForward dimension
+  d_k = d_v = 64  # dimension of K(=Q), V
+  n_layers = 6  # number of Encoder of Decoder Layer
+  n_heads = 8  # number of heads in Multi-Head Attention
+
+  model = Transformer()
+
+  criterion = nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+  enc_inputs, dec_inputs, target_batch = make_batch()
+
+  for epoch in range(20):
+      optimizer.zero_grad()
+      outputs, enc_self_attns, dec_self_attns, dec_enc_attns = model(enc_inputs, dec_inputs)
+      loss = criterion(outputs, target_batch.contiguous().view(-1))
+      print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+      loss.backward()
+      optimizer.step()
